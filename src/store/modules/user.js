7feb3c51378cab9c70b164f63cbe0ad392a4bb81
ifjiaru <50279853+ifjiaru@users.noreply.github.com>
2020-06-27 11:43:55 +1000

    Revert "Merge branch 'master' of https://github.com/ifjiaru/vue-element-admin"
    This reverts commit 2f5892ae169aec02a625cafb6cdc598f1799826c, reversing
    changes made to eb18455fb47743e4fc63567165eb24fa70b37c66.

--- src/store/modules/user.js
+++ src/store/modules/user.js
@@ -103,23 +103,28 @@ const actions = {
   },
 
   // dynamically modify permissions
-  async changeRoles({ commit, dispatch }, role) {
-    const token = role + '-token'
+  changeRoles({ commit, dispatch }, role) {
+    return new Promise(async resolve => {
+      const token = role + '-token'
 
-    commit('SET_TOKEN', token)
-    setToken(token)
+      commit('SET_TOKEN', token)
+      setToken(token)
 
-    const { roles } = await dispatch('getInfo')
+      const { roles } = await dispatch('getInfo')
 
-    resetRouter()
+      resetRouter()
 
-    // generate accessible routes map based on roles
-    const accessRoutes = await dispatch('permission/generateRoutes', roles, { root: true })
-    // dynamically add accessible routes
-    router.addRoutes(accessRoutes)
+      // generate accessible routes map based on roles
+      const accessRoutes = await dispatch('permission/generateRoutes', roles, { root: true })
 
-    // reset visited views and cached views
-    dispatch('tagsView/delAllViews', null, { root: true })
+      // dynamically add accessible routes
+      router.addRoutes(accessRoutes)
+
+      // reset visited views and cached views
+      dispatch('tagsView/delAllViews', null, { root: true })
+
+      resolve()
+    })
   }
 }
 
